9036eeb63e804f6ffdac95b2c0a7688a08ff8c7f
javierlnmn <jlm03123@gmail.com>
2024-10-15 12:08:12 +0200

    Voting models

--- voting/admin.py
+++ voting/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Vote, Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'slug', 'image')
+    search_fields = ('name', 'code')
+    list_filter = ('name', 'code')
+    
+    prepopulated_fields = {"slug": ("name",)}
+
+class VoteInlineFormSet(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        votes = {}
+        
+        for form in self.forms:
+            if form.cleaned_data and not form.cleaned_data.get('DELETE', False):
+                user = form.cleaned_data.get('user')
+                category = form.cleaned_data.get('category')
+                points = form.cleaned_data.get('points')
+                candidate = form.cleaned_data.get('candidate')
+
+                if (user, category) not in votes:
+                    votes[(user, category)] = {"points": set(), "candidates": set()}
+
+                if points in votes[(user, category)]["points"]:
+                    raise ValidationError(f'The user {user} has already assigned {points} points in this category.')
+
+                if candidate in votes[(user, category)]["candidates"]:
+                    raise ValidationError(f'The user {user} has already voted for {candidate} in this category.')
+
+                votes[(user, category)]["points"].add(points)
+                votes[(user, category)]["candidates"].add(candidate)
+
+        for (user, category), data in votes.items():
+            if len(data["points"]) > 3:
+                raise ValidationError(f'The user {user} cannot vote more than 3 times in the category {category}.')
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'candidate', 'category', 'points')
+    list_filter = ('category', 'user', 'candidate')
+    search_fields = ('user__username', 'candidate__name', 'category__name')
+    ordering = ('user', 'category', 'points')
+    
+    def save_model(self, request, obj, form, change):
+        obj.clean()
+        super().save_model(request, obj, form, change)
--- voting/models.py
+++ voting/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from candidates.models import Candidate
 
-# Create your models here.
+class Category(models.Model):
+    code = models.CharField(max_length=2, unique=True)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='category_images/')
+    description = models.TextField(null=True, blank=True)
+    winner = models.ForeignKey(Candidate, on_delete=models.PROTECT, related_name="won_categories", blank=True, null=True)
+    slug = models.SlugField(null=False, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return f'{self.name} ({self.code})'
+    
+    
+    
+class Vote(models.Model):
+
+    VOTING_POINTS_CHOICES = [
+        (1, '1'),
+        (2, '2'),
+        (3, '4'),
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    points = models.IntegerField(choices=VOTING_POINTS_CHOICES)
+    
+    class Meta:
+        unique_together = ["user", "candidate", "category"]
+        
+    def clean(self):
+        existing_vote = Vote.objects.filter(user=self.user, candidate=self.candidate, category=self.category)
+        if existing_vote.exists():
+            raise ValidationError('You have already voted for this candidate in this category.')
+
+        duplicate_points = Vote.objects.filter(user=self.user, category=self.category, points=self.points)
+        if duplicate_points.exists():
+            raise ValidationError(f'You have already assigned {self.get_points_display()} points in this category.')
+
+        votes_in_category = Vote.objects.filter(user=self.user, category=self.category).count()
+        if votes_in_category >= 3:
+            raise ValidationError('You have already voted 3 times in this category.')
+
+    def __str__(self):
+        return f"{self.user} voted {self.get_points_display()} points for {self.candidate} in {self.category}"
