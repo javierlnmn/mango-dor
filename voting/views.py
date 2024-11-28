49a234310bccdca8e5f949ff1c6460d8281c838a
javierlnmn <jlm03123@gmail.com>
2024-11-28 22:20:55 +0100

    Voting verification and error handling

--- voting/views.py
+++ voting/views.py
@@ -40,6 +40,24 @@ class VotingView(LoginRequiredMixin, View):
 
         return cached_categories
     
+    def validate_voting(self, voting_options_count, candidates_count, voting_points_count, candidate_vote_count):
+        # If there are repeated candidates
+        if voting_points_count != candidate_vote_count:
+            return False
+        
+        # If there are more candidates than available votes, and the recieved votes
+        # aren't the same as all available votes
+        if candidates_count > voting_options_count and voting_points_count != voting_options_count:
+            return False
+        
+        # If there are more or the same available votes than candidates, and the recieved votes
+        # aren't the number of candidates
+        if voting_options_count >= candidates_count and voting_points_count != candidates_count:
+            return False
+        
+        return True
+        
+    
     def get(self, request):
         current_category, current_category_number, total_categories_number = get_current_category_for_user(request.user)
         
@@ -68,9 +86,25 @@ class VotingView(LoginRequiredMixin, View):
         voting_points_list = [value for value, _ in Vote.VOTING_POINTS_CHOICES]
         voting_points_choices = {}
         for points in voting_points_list:
-            voting_points_choices[points] = request.POST.get(f'voting_choice_{points}_points', None)
+            try:
+                candidate_id = int(request.POST.get(f'voting_choice_{points}_points', None))
+                
+                # If candidate doesn't exists
+                if not Candidate.objects.filter(id=candidate_id).exists():
+                    return redirect('voting:voting')
+                
+                voting_points_choices[points] = candidate_id
+            except (ValueError, TypeError):
+                return redirect('voting:voting')
         
-        # Check if its a valid vote
+        candidates_count = Candidate.objects.all().count()
+        voting_points_count = len(set(voting_points_choices.keys()))
+        candidate_vote_count = len(set(voting_points_choices.values()))
+        
+        is_valid_voting = self.validate_voting(len(voting_points_list), candidates_count, voting_points_count, candidate_vote_count)
+        
+        if not is_valid_voting:
+            return redirect('voting:voting')
         
         for vote_points, vote_candidate in voting_points_choices.items():
             Vote.objects.create(
